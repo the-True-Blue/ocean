ec7dfe9028baa971d28a7466149f709b5c1ae709
cristianccgg <cristianccggg@gmail.com>
2025-03-25 13:09:13 -0500

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import Hero from "./components/hero/Hero";
+import Hero2 from "./components/hero/Hero2";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Hero2 />
+    </>
+  );
+}
+
+export default App;
